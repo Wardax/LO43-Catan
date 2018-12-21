0ad9d7f86206bd9875c1f72c92724f40d509c7c7
gponcion <guillaume.poncioni@gmail.com>
2018-12-21 13:49:35 +0100

    2

--- src/Vue.java
+++ src/Vue.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class Vue {
     private Partie p;
+    private VuePlateau[] vuesPlateaus;
+    private ArrayList<VueConstruction> VuesConstructions;
+    private ArrayList<VueJoueur> VuesJoueurs;
 
     public Vue(Partie p) {
         this.p = p;
